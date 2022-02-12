55b5de19dc29d6c3d938869b7888e506149c1566
Joy C <joy.cheng97324@gmail.com>
2022-02-12 15:52:48 +0800

    add fonts

--- src/views/backend/Dashboard.vue
+++ src/views/backend/Dashboard.vue
@@ -1,3 +1,21 @@
 <template>
+  <div class="">
+    <nav>
+      <div class="d-flex">
+        <div class="icon">menu</div>
+        <a href="" class="text-white">
+          <span class="material-icons-outlined"> widgets </span>
+          後台管理</a
+        >
+      </div>
+      <div class="d-flex">
+        <a href="#" class="d-flex">
+          <span class="material-icons"> home </span>
+          前往前台</a
+        >
+        <a href="#" class="d-flex">登出</a>
+      </div>
+    </nav>
     <router-view></router-view>
+  </div>
 </template>
