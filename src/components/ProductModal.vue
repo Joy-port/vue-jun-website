60b95cc3b1e464d5a51e5430a8967de56b3622f6
Joy C <joy.cheng97324@gmail.com>
2022-02-13 17:42:33 +0800

    edit product form

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -2,7 +2,7 @@
   <!-- Modal -->
   <div class="backdrop">
     <div class="modalContent h-100">
-      <div class="modal-content h-100">
+      <div class="modal-content shadow-none h-100">
         <div
           class="modal-header position-sticky top-0 border-0 bg-white"
           style="z-index: 1080"
@@ -16,6 +16,67 @@
           ></button>
         </div>
         <div class="modal-body text-break overflow-y-auto">
+          <div class="mb-5">
+            <label for="title" class="form-label mb-3">產品名稱</label>
+            <input type="text" class="form-control" v-model.lazy="product.title">
+          </div>
+          <div class="mb-5">
+            <label for="title" class="form-label mb-3">產品原價</label>
+            <input type="number" class="form-control" v-model.lazy.number="product.origin_price">
+          </div>
+          <div class="mb-5">
+            <label for="title" class="form-label mb-3">產品售價</label>
+            <input type="number" class="form-control" v-model.lazy.number="product.price">
+          </div>
+          <p class="mb-3">產品內容</p>
+          <div class="form-floating mb-5">
+            <textarea class="form-control" id="product-content" rows="5" v-model.lazy="product.content"></textarea>
+            <label for="product-content">輸入產品資訊</label>
+          </div>
+          <p class="mb-3">產品描述</p>
+          <div class="form-floating mb-5">
+            <textarea class="form-control" id="product-description" rows="5" v-model="product.description"></textarea>
+            <label for="product-content">描述此產品外觀、使用方式</label>
+          </div>
+          <div class="mb-5">
+            <label class="mb-3">產品數量</label>
+            <input type="number" class="form-control" v-model.lazy.number="product.number">
+          </div>
+          <div class="mb-5">
+            <label class="me-3">產品啟用</label>
+            <input type="checkbox" class="form-check-input" >
+          </div>
+          <div class="d-flex align-items-start mb-5">
+            <div class="mb-5 w-50">
+              <div class="mb-3">
+                <label class="mb-3">封面圖片</label>
+                <input type="text" class="form-control" v-model="imgUrl">
+              </div>
+              <label for="upload" class="btn btn-outline-primary rounded-sm" >
+                <input type="file" id="img" name="upload" class="d-none" accept="image/*">
+                <span class="material-icons text-base align-text-bottom"> file_upload </span>
+                上傳圖片
+              </label>
+            </div>
+            <div class="w-50 px-4">
+              <img :src="product.imageUrl || imgUrl" alt="上傳的圖片" class="rounded-md">
+            </div>
+          </div>
+
+<!--             <li
+              class="d-flex"
+              :class="{ 'text-danger': !product.is_enabled >= 1 }"
+            >
+              <span class="material-icons-outlined me-3"> shopping_cart </span>
+              <p v-if="product.is_enabled">
+                剩下 {{ product.is_enabled }} / {{ product.num }}
+                {{ product.unit }} 可販售
+              </p>
+              <p v-else>
+                沒有庫存囉！目前尚有 {{ product.num }} {{ product.unit }} 未出貨
+              </p>
+            </li> -->
+          <!-- img -->
           <div class="row mb-4 h-60">
             <div class="col-9 h-100">
               <div class="ratio-1x1 h-100">
@@ -32,53 +93,16 @@
               </div>
             </div>
           </div>
-          <ul class="container mb-0">
-            <li class="d-flex align-items-start mb-3">
-              <span class="badge bg-warning fs-6 me-3">{{
-                product.category
-              }}</span>
-              <input type="text" v-model.lazy="product.description">
-              <!-- <p class="mb-0 pe-5">{{ product.description }}</p> -->
-            </li>
-            <li class="d-flex mb-3">
-              <span class="material-icons-outlined me-3"> cake </span>
-              <input type="text" v-model.lazy="product.content">
-            </li>
-            <li class="d-flex">
-              <span class="material-icons-outlined me-3"> local_offer </span>
-              <p class="">
-                NT
-                <span class="text-decoration-line-through"
-                  >${{ product.origin_price }}</span
-                >
-                <input type="text" v-model.lazy="product.price"> /
-                {{ product.unit }}
-              </p>
-            </li>
-            <li
-              class="d-flex"
-              :class="{ 'text-danger': !product.is_enabled >= 1 }"
-            >
-              <span class="material-icons-outlined me-3"> shopping_cart </span>
-              <p v-if="product.is_enabled">
-                剩下 {{ product.is_enabled }} / {{ product.num }}
-                {{ product.unit }} 可販售
-              </p>
-              <p v-else>
-                沒有庫存囉！目前尚有 {{ product.num }} {{ product.unit }} 未出貨
-              </p>
-            </li>
-          </ul>
         </div>
         <div class="modal-footer border-0">
           <button
             type="button"
-            class="btn btn-outline-primary"
+            class="btn btn-outline-secondary"
             :data-id="product.id"
           >
             還原編輯
           </button>
-          <button type="button" class="btn btn-outline-danger" @click.prevent="editProduct(product)" >儲存</button>
+          <button type="button" class="btn btn-outline-success" @click.prevent="editProduct(product)" >儲存</button>
         </div>
       </div>
     </div>
@@ -92,7 +116,8 @@ export default {
   emits: ['toggle-modal'],
   data () {
     return {
-      product: null
+      product: null,
+      imgUrl: ''
     }
   },
   methods: {
